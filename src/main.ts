142a6164155ac46b8d38e4f864b42d477a14122e
ootidea <ootidea@gmail.com>
2021-07-06 18:56:43 +0900

    a要素が先頭に出現するパターンと末尾に出現するパターンの処理を実装

--- src/main.ts
+++ src/main.ts
@@ -36,10 +36,29 @@ inputArea.addEventListener('input', () => {
           const index = [...documentFragment.childNodes].indexOf(anchorElement)
           if (index === documentFragment.childNodes.length - 1) {
             // パターン2「テキスト <a>...</a>」
+            documentFragment.removeChild(anchorElement)
+            const dummy = document.createElement("dummy")
+            dummy.append(documentFragment)
+            outlineElement.setAttribute("text", dummy.innerHTML.trim())
 
+            const newElement = document.createElement("outline")
+            newElement.setAttribute("type", "link")
+            newElement.setAttribute("text", anchorElement.text)
+            newElement.setAttribute("url", anchorElement.href)
+            outlineElement.append(newElement)
           } else if (index === 0) {
             // パターン3「<a>...</a> テキスト」
+            outlineElement.setAttribute("type", "link")
+            outlineElement.setAttribute("text", anchorElement.text)
+            outlineElement.setAttribute("url", anchorElement.href)
 
+            documentFragment.removeChild(anchorElement)
+            const dummy = document.createElement("dummy")
+            dummy.append(documentFragment)
+            const newElement = document.createElement("outline")
+            newElement.setAttribute("text", dummy.innerHTML.trim())
+            newElement.setAttribute("html", dummy.innerHTML.trim())
+            outlineElement.append(newElement)
           } else {
             // パターン4「テキスト <a>...</a> テキスト」
 
