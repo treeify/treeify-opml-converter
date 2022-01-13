222fde9076624521dd1600ff0232b93afa1a5fcb
ootidea <ootidea@gmail.com>
2022-01-13 20:00:02 +0900

    全ファイルを削除してから npm init vite@latest を実行しsvelte-tsを選択

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'dist',
-  },
+  plugins: [svelte()]
 })
